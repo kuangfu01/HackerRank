52beea2a69455a0cab84ff73ae37889188346e23
kuangfu01 <fukuang@outlook.com>
2015-05-20 21:59:17 +0900

    Solution to Jumping Bunnies

---
+++ Jumping Bunnies/Program.fs
@@ -0,0 +1,35 @@
+﻿// https://www.hackerrank.com/challenges/jumping-bunnies
+open System
+
+let rec gcd (x:uint64) (y:uint64) : uint64 =
+    let min = Math.Min(x, y)
+    let max = Math.Max(x, y)
+    
+    if (max % min = 0UL) then min
+    else gcd min (max % min)    
+
+let lcm (list : uint64 list) =
+    match list with
+    | [] -> failwith "Use your brain !"
+    | [e] -> e
+    | x :: xs -> 
+        let rec loop l acc =
+            match l with
+            | [] -> acc
+            | x :: xs -> 
+                let t = gcd x acc
+                loop xs (acc / t * x)
+        loop xs x
+
+
+[<EntryPoint>]
+let main argv = 
+    Console.In.ReadLine() |> ignore
+    
+    Console.In.ReadLine().Split(' ') 
+    |> Array.toList 
+    |> List.map uint64
+    |> lcm
+    |> printfn "%d"
+    
+    0 
